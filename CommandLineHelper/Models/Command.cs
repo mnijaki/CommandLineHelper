7af79f077e255491dfa252d57af1ec9909849b28
mnijaki <mnijaki@gmail.com>
2024-01-18 12:48:17 +0100

    * Added main model that represents Command

---
+++ CommandLineHelper/Models/Command.cs
@@ -0,0 +1,10 @@
+namespace CommandLineHelper.Models
+{
+	public class Command
+	{
+		public int Id { get; set; }
+		public string Description { get; set; }
+		public string CommandLine { get; set; }
+		public string Platform { get; set; }
+	}
+}
