a5ad59331f4eac107ec3040e13059a1c599db54c
mnijaki <mnijaki@gmail.com>
2024-01-18 12:31:26 +0100

    * Cleaned code in files * Added some comments

--- CommandLineHelper/Startup.cs
+++ CommandLineHelper/Startup.cs
@@ -1,34 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace CommandLineHelper
 {
 	public class Startup
 	{
+		public IConfiguration Configuration { get; }
+		
 		public Startup(IConfiguration configuration)
 		{
 			Configuration = configuration;
 		}
 
-		public IConfiguration Configuration { get; }
-
-		// This method gets called by the runtime. Use this method to add services to the container.
+		/// <summary>
+		///   This method gets called by the runtime.
+		///   Use this method to add services to the container.
+		///   Add services that will be used by your application here.
+		/// </summary>
+		/// <param name="services"></param>
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		/// <summary>
+		///   This method gets called by the runtime.
+		///   Use this method to configure the HTTP request pipeline.
+		///   Add middleware that will be used by your application here.
+		///   Add code here that will be responsible for setting up the HTTP request pipeline.
+		///   Request pipeline is made up of multiple middleware components.
+		///   Each middleware component is responsible for performing a specific function.
+		///   The order in which the middleware components are added in this method is important.
+		///   Each middleware may or may not pass the request to the next middleware component in the pipeline.
+		///   If it does not pass the request to the next middleware component, then it is responsible for generating the response for the request.
+		/// </summary>
+		/// <param name="app"></param>
+		/// <param name="env"></param>
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
 		{
 			if(env.IsDevelopment())
