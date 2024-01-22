6ff117284c7154eb2c57739d23607fb43e18c857
mnijaki <mnijaki@gmail.com>
2024-01-22 23:20:32 +0100

    * Added necessary restrictions to model * Added migrations * Pushed migrations down to DB * Created some data in DB table

--- CommandLineHelper/Models/Command.cs
+++ CommandLineHelper/Models/Command.cs
@@ -1,10 +1,21 @@
 namespace CommandLineHelper.Models
 {
+	using System.ComponentModel.DataAnnotations;
+
 	public class Command
 	{
+		[Key]
+		[Required]
 		public int Id { get; set; }
+		
+		[Required]
 		public string Description { get; set; }
+		
+		[Required]
+		[MaxLength(250)]
 		public string CommandLine { get; set; }
+		
+		[Required]
 		public string Platform { get; set; }
 	}
 }
